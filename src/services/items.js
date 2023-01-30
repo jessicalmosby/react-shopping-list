ce4e9603dafb7a4e8d3ecb3e970d508b6a35ae0c
Jessica Mosby <jessicamosby@mac.com>
2023-01-30 15:12:12 -0500

    added link to my supabase and changed name of list in items.js to match mine

--- src/services/items.js
+++ src/services/items.js
@@ -1,14 +1,14 @@
 import { checkError, client } from './client';
 
 export async function getListItems() {
-  const response = await client.from('shopping_items').select();
+  const response = await client.from('shopping_list').select();
   // this will only grab items that belong to this user thanks to RLS and user_id property
 
   return checkError(response);
 }
 
 export async function createListItem(name, qty) {
-  const response = await client.from('shopping_items').insert([{ name, qty }]).single(); // because of RLS and our default values, we add user_id for free
+  const response = await client.from('shopping_list').insert([{ name, qty }]).single(); // because of RLS and our default values, we add user_id for free
 
   return checkError(response);
 }
@@ -19,7 +19,7 @@ export async function createListItem(name, qty) {
 export async function toggleListItem({ id, purchased }) {
   // sets a given list item's bought property to true
   const response = await client
-    .from('shopping_items')
+    .from('shopping_list')
     .update({ purchased: !purchased })
     .match({ id })
     .single();
